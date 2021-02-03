ccf7528a4fdd67cd578adb368407e1e096159758
mika1407 <koneautomaatio@hotmail.com>
2021-02-03 16:27:47 +0200

    Add task submit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
 
 const App = () => {
-    const [tasks,setTasks] = useState([
+  const [showAddTask, setShowAddTask] = useState (false)
+  const [tasks,setTasks] = useState([
   {
     id: 1,
     text: 'Doctors Appointment',
@@ -25,6 +26,13 @@ const App = () => {
   }
 ])
 
+// Add Task
+const addTask = (task) => {
+  const id = Math.floor(Math.random() * 10000) + 1
+  const newTask = { id, ...task}
+  setTasks([...tasks, newTask])
+}
+
 // Delete Task
 const deleteTask = (id) => {
   setTasks(tasks.filter((task) => task.id !==id))
@@ -38,7 +46,7 @@ const toggleReminder = (id) => {
 return (
     <div className="container">
       <Header />
-      <AddTask />
+      {showAddTask && <AddTask onAdd={addTask}/>}
       {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask}
       onToggle={toggleReminder}/>): ('No Tasks To Show')}
     </div>
